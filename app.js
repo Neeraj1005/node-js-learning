4366e60d58389543e6ae48cbb1af357c069ce9c1
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-08-20 16:40:20 +0530

    handle http route

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
-
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello Earth!'
-  }));
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Hello Earth!",
+      })
+    );
+  }
+
+  if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "About Page",
+      })
+    );
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Page Not Found",
+      })
+    );
+  }
 });
 
 console.log(`server is running at http://localhost:${PORT}`);
