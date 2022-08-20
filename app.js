c11771df322afd292f78c5daa0fc841db2e83df1
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-08-20 16:47:16 +0530

    show html content

--- app.js
+++ app.js
@@ -5,28 +5,19 @@ const PORT = 3000;
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello Earth!",
-      })
-    );
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("page/home.html");
+    res.end();
   }
 
   if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "About Page",
-      })
-    );
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Page Not Found",
-      })
-    );
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
   }
 });
 
