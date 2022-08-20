1cb8368262030ae973d26b9f7752aa43c25ca332
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-08-20 16:15:22 +0530

    understanding common js module

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const http = require('http');
+const Circle = require('./circle');
 
-// Create a local server to receive data from
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!'
-  }));
-});
 
-server.listen(3000);
+const circle = new Circle();
+
+console.log(circle.area(5));
+console.log(circle.circumference(5));
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+
+
+// // Create a local server to receive data from
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(JSON.stringify({
+//     data: 'Hello World!'
+//   }));
+// });
+
+// server.listen(3000);
