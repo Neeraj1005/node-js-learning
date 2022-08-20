405a62b758488f0ee3897f0d140f1b0893a55526
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-08-20 16:29:10 +0530

    package json crated

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
-console.log(__filename)
-console.log(__dirname)
+const http = require('http');
+
+const PORT = 3000;
 
 
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!'
+  }));
+});
 
-
-
-
-// const Circle = require('./circle');
-
-
-// const circle = new Circle();
-
-// console.log(circle.area(5));
-// console.log(circle.circumference(5));
-
-
-
-
-
-
-
-
-
-
-
-// const http = require('http');
-
-
-
-// // Create a local server to receive data from
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify({
-//     data: 'Hello World!'
-//   }));
-// });
-
-// server.listen(3000);
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
